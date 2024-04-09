ebc13e5e78e02109ffee28f8daffa2e0535c9623
James Antrobus <jamesantrobus@gmail.com>
2024-04-09 13:36:32 +0100

    Add rough list of search results

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+import SearchResult from "@/components/SearchResult"
 import BeerMap from "@/components/BeerMap"
 
 export default function Home() {
   return (
     <main className="">
+      <div className="absolute z-10 top-5 left-5 bottom-10 bg-white w-80 rounded-md shadow-md">
+        <div className="pt-3 pl-3 pr-3 space-y-3">
+          <SearchResult />
+          <SearchResult />
+          <SearchResult />
+          <SearchResult />
+          <SearchResult />
+          <SearchResult />
+          <SearchResult />
+        </div>
+      </div>
+
       <div className="w-screen h-screen">
         <BeerMap />
       </div>
---
+++ src/components/SearchResult.tsx
@@ -0,0 +1,18 @@
+export default function SearchResult() {
+    return (
+        <>
+            <div className="flex justify-between">
+                <div>
+                    <div className="font-semibold text-xl">Brewdog</div>
+                    <div className="text-md">⭐️ ⭐️ ⭐️ ⭐️ ⭐️</div>
+                </div>
+                <div>
+                    TODO
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <div className="w-9/12 h-px bg-gray-200"></div>
+            </div>
+        </>
+  )
+}
