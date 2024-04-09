9b30b1db6090d1232c27039dc4abd4c05ed0500c
James Antrobus <jamesantrobus@gmail.com>
2024-04-09 14:08:48 +0100

    Add FontAwesome, CTA for search results

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons"
+
 export default function SearchResult() {
     return (
         <>
@@ -6,8 +9,8 @@ export default function SearchResult() {
                     <div className="font-semibold text-xl">Brewdog</div>
                     <div className="text-md">⭐️ ⭐️ ⭐️ ⭐️ ⭐️</div>
                 </div>
-                <div>
-                    TODO
+                <div className="w-8 flex justify-center">
+                    <FontAwesomeIcon icon={faCircleArrowRight} />
                 </div>
             </div>
             <div className="flex justify-center">
