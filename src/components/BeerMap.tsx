1a7b8b94e831625647fd5d1b6317dbcbfd3d1395
James Antrobus <jamesantrobus@gmail.com>
2024-04-10 10:42:21 +0100

    Refactor map component

--- src/components/BeerMap.tsx
+++ src/components/BeerMap.tsx
@@ -12,32 +12,16 @@ type BeerMapProps = {
 }
 
 const BeerMap: React.FC<BeerMapProps> = ({ venues = [], selectedVenue, onSelect }) => {
-  // TODO extract default state
-  const [viewState, setViewState] = useState({
-    longitude: -1.5448842044972366,
-    latitude: 53.797900782787224,
-    zoom: 14
-  });
+  // setup the default state to be Leeds City Centre
+  const defaultState = { longitude: -1.5448842044972366, latitude: 53.797900782787224, zoom: 14 };
+  const [viewState, setViewState] = useState(defaultState);
 
   useEffect(() => {
-    if (selectedVenue) {
-      setViewState({ latitude: selectedVenue.location.latitude, longitude: selectedVenue.location.longitude, zoom: 16 })
-    } else {
-      // TODO review conditional, use default state if needed
-
-      setViewState({
-        longitude: -1.5448842044972366,
-        latitude: 53.797900782787224,
-        zoom: 14
-      })
-    }
+    // handle a venue being selected (show details) or unselected (revert to default)
+    setViewState(selectedVenue ? 
+      { latitude: selectedVenue.location.latitude, longitude: selectedVenue.location.longitude, zoom: 16 } 
+      : defaultState)
   }, [selectedVenue])
-
-  const onInternalSelect = (venue: Venue) => {
-    // TODO
-    // setViewState({ latitude: venue.location.latitude, longitude: venue.location.longitude, zoom: 16 })
-    onSelect(venue)
-  }
 
   return (
     <Map
@@ -48,7 +32,7 @@ const BeerMap: React.FC<BeerMapProps> = ({ venues = [], selectedVenue, onSelect 
       {...viewState}
     >
       {venues.map((venue, index) => (
-        <Marker key={index} longitude={venue.location.longitude} latitude={venue.location.latitude} anchor="bottom" onClick={() => onInternalSelect(venue)}>
+        <Marker key={index} longitude={venue.location.longitude} latitude={venue.location.latitude} anchor="bottom" onClick={() => onSelect(venue)}>
           <img className={`w-10 h-10 ${(!selectedVenue || selectedVenue.name === venue.name) ? 'opacity-100' : 'opacity-65'}`} src="./map-marker.png" />
         </Marker>
       ))}
